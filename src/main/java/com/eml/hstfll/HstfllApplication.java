417e5e4867a0bcb58467009ea78a5b8665c7c8a0
Esteves <esteves_m_lima@hotmail.com>
2023-11-15 15:17:13 -0300

    feat - project init

---
+++ src/main/java/com/eml/hstfll/HstfllApplication.java
@@ -0,0 +1,13 @@
+package com.eml.hstfll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HstfllApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HstfllApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eml/hstfll/HstfllApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eml.hstfll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HstfllApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
